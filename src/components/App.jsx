27a71789d0b8042b7aa262b55cfbeb44e526e37b
SergeyZu <vechniystudent19@yandex.ru>
2023-04-30 07:21:04 +0700

    Настроил переадресацию в зависимости от статуса авторизации

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,37 @@
-import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import CardsPage from './CardsPage';
 import Register from './Register';
 import Login from './Login';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <Routes>
-      <Route path="/" element={<CardsPage />} />
+      {/* <Route path="/" element={<CardsPage />} /> */}
+      <Route
+        path="/"
+        element={!loggedIn ? <Navigate to="/sign-in" /> : <CardsPage />}
+      />
       <Route path="/sign-up" element={<Register />} />
       <Route path="/sign-in" element={<Login />} />
+      {/* <Route
+        path="/sign-up"
+        element={
+          <div className="registerContainer">
+            <Register />
+          </div>
+        }
+      />
+      <Route
+        path="/sign-in"
+        element={
+          <div className="loginContainer">
+            <Login />
+          </div>
+        }
+      /> */}
     </Routes>
   );
 }
