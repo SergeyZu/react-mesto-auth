1a4beae80ff17544251adc44e071573591a7f746
SergeyZu <vechniystudent19@yandex.ru>
2023-04-30 15:24:00 +0700

    Настроил HOC ProtectedRoute

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,26 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import CardsPage from './CardsPage';
 import Register from './Register';
 import Login from './Login';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <Routes>
-      {/* <Route path="/" element={<CardsPage />} /> */}
       <Route
         path="/"
-        element={!loggedIn ? <Navigate to="/sign-in" /> : <CardsPage />}
+        element={
+          !loggedIn ? (
+            <Navigate to="/sign-in" replace />
+          ) : (
+            <Navigate to="/cards" replace />
+          )
+        }
+      />
+      <Route
+        path="/cards"
+        element={<ProtectedRoute element={<CardsPage />} loggedIn={loggedIn} />}
       />
       <Route path="/sign-up" element={<Register />} />
       <Route path="/sign-in" element={<Login />} />
